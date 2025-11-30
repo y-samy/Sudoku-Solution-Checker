19d8b9020b1403783122efa2ef847a0ffb77906e
Youssef Samy <office.youssefkassem@gmail.com>
2025-11-30 17:41:49 +0200

    Unused import

--- sudokusolutionchecker/src/main/java/com/github/y_samy/validation/base/BoardChecker.java
+++ sudokusolutionchecker/src/main/java/com/github/y_samy/validation/base/BoardChecker.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
